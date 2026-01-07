40eac98b99ad466be9594c91803f7f02a3425092
Éderson <edersonramos952@gmail.com>
2026-01-07 17:18:00 -0300

    deploy frontend

--- frontend/src/assets/js/config.js
+++ frontend/src/assets/js/config.js
@@ -1,4 +1,4 @@
 const API_URL =
     window.location.hostname === "localhost"
         ? "http://localhost:3000"
-        : "https://SEU-BACKEND.onrender.com";
+        : "https://backend-carbono.onrender.com";
